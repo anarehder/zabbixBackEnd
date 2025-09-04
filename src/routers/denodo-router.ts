9ca269973d1ac0d8ddd63866862fb4b152cd51f9
anarehder <anapaula.rehder@gmail.com>
2025-09-04 11:54:49 +0000

    feat: firewall js

--- src/routers/denodo-router.ts
+++ src/routers/denodo-router.ts
@@ -6,7 +6,7 @@ import { getDBDataJSPecas, getFirewallJSPecas, getJiraJSPecas, getLinksJSPecas }
 const denodoRouter = Router();
 
 denodoRouter.post("/monitoramento/links/jspecas", validateBody(monthSchema), getLinksJSPecas);
-denodoRouter.post("/monitoramento/firewakk/jspecas", validateBody(monthSchema), getFirewallJSPecas);
+denodoRouter.post("/monitoramento/firewall/jspecas", validateBody(monthSchema), getFirewallJSPecas);
 denodoRouter.get("/monitoramento/db/jspecas", getDBDataJSPecas);
 denodoRouter.post("/monitoramento/jira/jspecas", validateBody(monthSchema), getJiraJSPecas);
 
